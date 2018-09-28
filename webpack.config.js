bf1dbd0532d26a2cbcc643c3c2f9c55c1dfc1865
theallmightyjohn <john@vulgarkittenstudios.tk>
2018-09-28 16:51:05 -0400

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,32 @@
-const webpack = require('webpack'); const 
-devMode = process.env.NODE_ENV !== 
-'production'; const MiniCssExtractPlugin = 
-require("mini-css-extract-plugin"); 
 module.exports = {
-				mode: 
-'development',
-    context: __dirname,
-    entry: __dirname+"/main.js",
-    output: {
-        path: __dirname+"/dist",
-        filename: 'bundle.js'
-    },
-    module: {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: 
-/(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
+  mode: 'development',
+  context: __dirname,
+  entry: __dirname+"/main.js",
+  output: {
+      libraryTarget: 'commonjs',
+      path: __dirname+"/dist",
+      filename: 'raiden.js'
+  },
+  externals: {
+    'tiny-ecs': {
+      commonjs: 'tiny-ecs',
+      commonjs2: 'tiny-ecs',
+      amd: 'tiny-ecs',
+      root: 'tiny-ecs'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
         }
       }
-    },
-    {
-        test: /\.s?[ac]ss$/,
-        use: [
-          devMode ? 'style-loader' : 
-MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
-        ],
-      }
-  ]
-}
+    ]
+  }
 };
